ea7e1cccd87c5e6b12e5bed3d60465eda2d8afb4
Anette Rivera <anettem.rivera@gmail.com>
2020-05-05 03:01:01 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,16 +1,19 @@
 def unsafe?(speed)
   if speed > 40 && speed < 60
-    return FALSE
+    return TRUE
   else 
+    return FALSE
 end 
 
 end
 
 
+speed(50)
 
-def not_safe?(speed)
+
+#def not_safe?(speed)
 	
-end
+#end
 	
 
 
